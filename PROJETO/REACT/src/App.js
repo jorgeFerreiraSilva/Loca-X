61c8bf325056e709d2d967ee0af9eb37ad3eda70
Marianna <mddobal@gmail.com>
2019-03-03 11:20:35 -0300

    select state component created + home page started

--- PROJETO/REACT/src/App.js
+++ PROJETO/REACT/src/App.js
@@ -2,14 +2,15 @@ import React, { Component } from 'react';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
 import Header from './components/Header';
+import Home from './pages/Home';
+import SelectState from './components/SelectState';
 
 class App extends Component {
   render() {
     return (
       <div>
-      <Header />
-      <Login />
       Hello {this.props.name}
+      <Home />
       </div>
     );
   }
