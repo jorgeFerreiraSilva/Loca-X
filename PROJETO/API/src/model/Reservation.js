83951600fb37bc7fc673069e3361432dc208a24c
BarbieriMatheus <abarbierijunior@gmail.com>
2019-03-06 13:46:30 -0300

    Ads, reservation, message functionalities

---
+++ PROJETO/API/src/model/Reservation.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const reservationSchema = new Schema({
+  adId: { type: mongoose.Schema.Types.ObjectId, ref: 'Ad', required: true },
+  hirerId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  ownerId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  pricePerDay: { type: Number, min: 1 },
+  startDate: { type: Date },
+  endDate: { type: Date },
+  status: { type: String, required: true, default: 'Em espera', enum: ['Em espera', 'Alugando', 'Finalizado', 'Recusado'] }
+});
+
+const Reservation = mongoose.model('Reservation', reservationSchema);
+module.exports = Reservation;
+
+
+
+
+
