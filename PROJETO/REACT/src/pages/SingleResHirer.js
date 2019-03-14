2638ce8ddfb88511a3589eb80a9fcbfee10500a1
Marianna <mddobal@gmail.com>
2019-03-14 18:06:46 -0300

    show reservation status

--- PROJETO/REACT/src/pages/SingleResHirer.js
+++ PROJETO/REACT/src/pages/SingleResHirer.js
@@ -9,6 +9,8 @@ import Row from 'react-bootstrap/Row';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
+import Image from 'react-bootstrap/Image';
+import './css/Product.css';
 
 const styles = theme => ({
   
@@ -19,11 +21,7 @@ const styles = theme => ({
 
   left: {
     width: '45%'
-  },
-
-  // box: {
-  //   marginBottom: '5%'
-  // }
+  }
 });
 
 class SingleResHirer extends Component {
@@ -39,6 +37,8 @@ class SingleResHirer extends Component {
       endDate: '',
       hirerId: '',
       ownerId: '',
+      ownerName: '',
+      ownerPic: '',
       status: '',
       itemId: ''
     };
@@ -57,15 +57,16 @@ class SingleResHirer extends Component {
       .catch(err => console.log(err));
     axios.get(`http://192.168.0.41:8080/api/reservation/${this.props.match.params.id}`)
       .then((response) => {
-        const { totalPrice, startDate, endDate, hirerId, ownerId } = response.data[0];
-        this.setState({ totalPrice, startDate, endDate, hirerId, ownerId });
+        const { totalPrice, startDate, endDate, hirerId, ownerId, status } = response.data[0];
+        this.setState({ totalPrice, startDate, endDate, hirerId, ownerId, status });
         console.log('RESERVATION RESPONSE', response);
         axios.get(`http://192.168.0.41:8080/api/users/${this.state.ownerId}`)
           .then((response) => {
-              // const { name, pathPicture } = response.data[0];
-              // this.setState({ totalPrice, startDate, endDate, hirerId, ownerId });
-              console.log('OWNER DATA>>>>>', response);
-            });
+            const ownerName = response.data.name;
+            const ownerPic = response.data.pathPicture;
+            this.setState({ ownerName, ownerPic });
+            console.log('OWNER DATA>>>>>', response.data);
+          });
       });
   }
 
@@ -86,42 +87,57 @@ class SingleResHirer extends Component {
     
     const { classes } = this.props;
     return (
-      <div>
-        <h1>AQUI</h1>
+      <div className='app'>
       <Container>
-        <Row>
-          <Col>
-          <h1>Infos do anuncio</h1>
-            <Card style={{ width: '18rem' }}>
-              <Card.Img variant="top" src={this.state.pathPictures[0]} />
+        <Row className="justify-content">
+
+          <Col xs={12} md={6} className="margin-bottom-5">
+            <Card className='margin-bottom-5 align-item padding-5 border-shadow'>
+              <Card.Img className="w-50" variant="top" src={this.state.pathPictures[0]} alt="product" />
+            </Card>
+            <Card>
+              <Card.Header><b>{this.state.title}</b></Card.Header>
               <Card.Body>
-                <Card.Title>{this.state.title}</Card.Title>
                 <Card.Text>
-                {this.state.description}
+                  {this.state.description}
                 </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+
+          <Col xs={12} md={4}>
+            <Card>
+              <Card.Header><b>Informações da reserva</b></Card.Header>
+              <Card.Body>
+                <div className="margin-bottom-5 padding-bottom-5 border-bottom">
+                  <Card.Text>
+                  <span style={{ "font-size": '16px' }}> Status: </span> <strong>{this.state.status}</strong><hr/>
+                    <span style={{ "font-size": '16px' }}> Início: </span> <strong>{this.state.startDate}</strong><br/>
+                    <span style={{ "font-size": '16px' }}> Término: </span> <strong>{this.state.endDate}</strong><hr/>
+                    <span style={{ "font-size": '16px' }}> Total: </span> <strong>R$ {this.state.totalPrice}</strong> </Card.Text>
+                </div>
+              </Card.Body>
+            </Card>
+
+            <Card className="text-center margin-top-bottom-5 padding-5" >
+              <div> 
                 <Link to={{
-                  pathname: `/produto/${this.state.itemId}`
-                }}
-                >
-                <Button variant="primary">Ver página do produto</Button>
+                  pathname: `/perfil/${this.state.ownerId}`
+                }}>
+                  <Image style={{ width: "130px" }} src={this.state.ownerPic} roundedCircle />
                 </Link>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col>
-            <Card style={{ width: '20rem' }}>
-              <Card.Body>
-                <Card.Text>
-                    De: {this.state.startDate}<br/>
-                    Até: {this.state.endDate}<br/>
-                    Total: R${this.state.totalPrice}
+              </div>
+              <div>
+                <Card.Text>Anunciado por:
+                {' '} {this.state.ownerName}
                 </Card.Text>
-              </Card.Body>
+              </div>
             </Card>
           </Col>
+
         </Row>
       </Container>
-    </div>
+    </div >
     );
   }
 }
--- PROJETO/REACT/src/pages/SingleResOwner.js
+++ PROJETO/REACT/src/pages/SingleResOwner.js
@@ -7,6 +7,8 @@ import Row from 'react-bootstrap/Row';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
+import Image from 'react-bootstrap/Image';
+import './css/Product.css';
 
 class SingleResOwner extends Component {
   constructor(props) {
@@ -20,6 +22,8 @@ class SingleResOwner extends Component {
       startDate: null,
       endDate: null,
       hirerId: null,
+      hirerName: '',
+      hirerPic: '',
       status: null,
       itemId: null
     };
@@ -41,9 +45,16 @@ class SingleResOwner extends Component {
       .catch(err => console.log(err));
     axios.get(`http://192.168.0.41:8080/api/reservation/${this.props.match.params.id}`)
       .then((response) => {
-        const { totalPrice, startDate, endDate, hirerId } = response.data[0];
-        this.setState({ totalPrice, startDate, endDate, hirerId });
-        console.log('RESERVATION RESPONSE', response)
+        const { totalPrice, startDate, endDate, hirerId, status } = response.data[0];
+        this.setState({ totalPrice, startDate, endDate, hirerId, status });
+        console.log('RESERVATION RESPONSE', response);
+        axios.get(`http://192.168.0.41:8080/api/users/${this.state.hirerId}`)
+          .then((response) => {
+            const hirerName = response.data.name;
+            const hirerPic = response.data.pathPicture;
+            this.setState({ hirerName, hirerPic });
+            console.log('OWNER DATA>>>>>', response.data);
+          });
       });
   }
 
@@ -64,7 +75,7 @@ class SingleResOwner extends Component {
       .then((response) => {
         const { status } = response.data;
         // this.setState({ status });
-        console.log("Alugado");
+        console.log('Alugado');
       })
       .catch(err => console.log(err));
   }
@@ -82,44 +93,76 @@ class SingleResOwner extends Component {
   //   .catch(err => console.log(err));
   // };
 
-  render () {
+  render() {
     return (
-      <div>
+      <div className="app">
         <Container>
-          <Row>
-            <Col>
-            <h1>Infos do anuncio</h1>
-              <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src={this.state.pathPictures[0]} />
-                <Card.Body>
-                  <Card.Title>{this.state.title}</Card.Title>
-                  <Card.Text>
+        <Row className="justify-content">
+
+          <Col xs={12} md={6} className="margin-bottom-5">
+            <Card className="margin-bottom-5 align-item padding-5 border-shadow">
+              <Card.Img className="w-50" variant="top" src={this.state.pathPictures[0]} alt="product" />
+            </Card>
+            <Card>
+              <Card.Header><b>{this.state.title}</b></Card.Header>
+              <Card.Body>
+                <Card.Text>
                   {this.state.description}
-                  </Card.Text>
-                  <Link to={{
-                    pathname: `/produto/${this.state.itemId}`
-                  }}
-                  >
-                  <Button variant="primary">Ver página do produto</Button>
-                  </Link>
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col>
-              <Card style={{ width: '20rem' }}>
-                <Card.Body>
-                  {/* <Card.Title>Card Title</Card.Title>
-                  <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle> */}
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+
+          <Col xs={12} md={4}>
+            <Card>
+              <Card.Header><b>Informações da reserva</b></Card.Header>
+              <Card.Body>
+                <div className="margin-bottom-5 padding-bottom-5 border-bottom">
                   <Card.Text>
-                      De: {this.state.startDate}<br/>
-                      Até: {this.state.endDate}<br/>
-                      Total: R${this.state.totalPrice}
+                    <span style={{ 'font-size': '16px' }}> Status: </span> 
+                    {' '}
+                    <strong>{this.state.status}</strong>
+                    <hr/>
+                    <span style={{ 'font-size': '16px' }}> Início: </span> 
+                    {' '}
+                    <strong>{this.state.startDate}</strong>
+                    <br/>
+                    <span style={{ 'font-size': '16px' }}> Término: </span> 
+                    {' '}
+                    <strong>{this.state.endDate}</strong>
+                    <hr/>
+                    <span style={{ 'font-size': '16px' }}> Total: </span> 
+                    {' '}
+                    <strong>R$ {this.state.totalPrice}</strong>
+                    {' '}
+                    
                   </Card.Text>
-                </Card.Body>
-              </Card>
-            </Col>
-          </Row>
-        </Container>
+                </div>
+              </Card.Body>
+            </Card>
+
+            <Card className="text-center margin-top-bottom-5 padding-5">
+              <div>
+                <Link to={{
+                  pathname: `/perfil/${this.state.hirerId}`
+                }}
+                >
+                  <Image style={{ width: '130px' }} src={this.state.hirerPic} roundedCircle />
+                </Link>
+              </div>
+              <div>
+                <Card.Text>
+                  Reservado por:
+                  {' '} 
+                  {' '}
+                  {this.state.hirerName}
+                </Card.Text>
+              </div>
+            </Card>
+          </Col>
+
+        </Row>
+      </Container>
       </div>
     );
   }
